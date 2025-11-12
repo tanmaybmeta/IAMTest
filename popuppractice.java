d918e32fc5ced22b9937e8bae7991ad0ea742045
tanmaybmeta <tanmay.b@metaphi.in>
2025-11-12 17:38:35 +0530

    popup updated code

--- popuppractice.java
+++ popuppractice.java
@@ -1,7 +1,14 @@
 package testngpractice;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
 import java.nio.channels.SelectableChannel;
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -12,7 +19,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 
 public class popuppractice {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException, AWTException {
 		
 		
 //		1.alert and javascript popup code
@@ -94,20 +101,128 @@ public class popuppractice {
 //		Thread.sleep(3000);
 //		d.close();
 		
+	//	5.file upload popup code
+//		WebDriver d = new ChromeDriver();
+//		d.manage().window().maximize();
+//		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		
+//		d.get("https://im.dev.metaphi.in/login");
+//		d.findElement(By.id("formUsername")).sendKeys("admin@gmail.com");
+//		d.findElement(By.id("formPassword")).sendKeys("123");
+//		
+//		d.findElement(By.xpath("//button[.=' Sign In']")).click();
+//		
+//		d.findElement(By.xpath("//span[.='Procurement']")).click();
+//		d.findElement(By.linkText("Purchase Request")).click();
+//		d.findElement(By.linkText("Create Purchase Request")).click();
+//		
+//		//date
+//		WebElement date = d.findElement(By.xpath("//input[@type='date']"));
+//		date.clear();
+//		date.sendKeys("13/11/2025");
+//		
+//		//file
+//		File f = new File("src/testngpractice/file.txt");
+//		
+//		String path = f.getAbsolutePath();
+//		System.out.println("path: "+path);
+//		
+//		d.findElement(By.xpath("//input[@type='file']")).sendKeys(path);
+//		
+//		d.findElement(By.xpath("//input[@name='initiated_by']")).sendKeys("Tanmay b");
+//		
+//		d.findElement(By.name("remark")).sendKeys("Test automation");
+//		
+//		//item details form
+//		d.findElement(By.xpath("//input[@placeholder='Select']")).sendKeys("Acer");
+//		d.findElement(By.xpath("//li[.='Acer']")).click();
+//		
+//		d.findElement(By.xpath("//input[@type='number']")).sendKeys("10");
+//		
+//		d.findElement(By.xpath("(//input[@placeholder='Enter Remark'])[2]")).sendKeys("Test Automation");
+//		
+//		d.findElement(By.xpath("//button[.='Submit']")).submit();
+//		
+//		Thread.sleep(3000);
+//		
+//		d.close();
 		
-		WebDriver d = new ChromeDriver();
-		d.manage().window().maximize();
-		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
-		d.get("https://im.dev.metaphi.in/login");
-		d.findElement(By.id("formUsername")).sendKeys("admin@gmail.com");
-		d.findElement(By.id("formPassword")).sendKeys("123");
+//		Runtime.getRuntime().exec("Notepad");
+//		Thread.sleep(2000);
+//		
+//		Robot r = new Robot();
+//		r.keyPress(KeyEvent.VK_T);
+//		r.keyPress(KeyEvent.VK_A);
+//		r.keyPress(KeyEvent.VK_N);
+//		r.keyPress(KeyEvent.VK_M);
+//		r.keyPress(KeyEvent.VK_A);
+//		r.keyPress(KeyEvent.VK_Y);
 		
-		d.findElement(By.xpath("//button[.=' Sign In']")).click();
+	//	6. file download popup code
+//		WebDriver d = new ChromeDriver();
+//		
+//		d.manage().window().maximize();
+//		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		
+//		d.get("https://im.dev.metaphi.in/login");
+//		
+//		Robot r = new Robot();
+//		r.keyPress(KeyEvent.VK_CONTROL);
+//		r.keyPress(KeyEvent.VK_S);
+//		r.keyRelease(KeyEvent.VK_CONTROL);
+//		Thread.sleep(3000);
+//		r.keyPress(KeyEvent.VK_TAB);
+//		r.keyPress(KeyEvent.VK_TAB);
+//		r.keyPress(KeyEvent.VK_TAB);
+//		r.keyPress(KeyEvent.VK_ENTER);
+//		r.keyPress(KeyEvent.VK_TAB);
+//		r.keyPress(KeyEvent.VK_ENTER);
+//		Thread.sleep(3000);
+//		d.close();
 		
-		d.findElement(By.xpath("//span[.='Procurement']")).click();
-		d.findElement(By.linkText("Purchase Request")).click();
-		d.findElement(By.linkText("Create Purchase Request")).click();
+	//	7. windowhandle and windowhandles method code
+		
+//		WebDriver d = new ChromeDriver();
+//		d.manage().window().maximize();
+//		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		
+//		d.get("https://im.dev.metaphi.in/login");
+//		String winid = d.getWindowHandle();
+//		System.out.println(winid);
+//		
+//		d.close();
+		
+//		
+//		WebDriver d = new ChromeDriver();
+//		d.manage().window().maximize();
+//		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		
+//		d.get("https://demoapps.qspiders.com/ui?scenario=1");
+//		
+//		String pwin = d.getWindowHandle();
+//		
+//		d.findElement(By.xpath("//section[.='Popups']")).click();
+//		d.findElement(By.xpath("//section[.='Browser Windows']")).click();
+//		d.findElement(By.linkText("Multiple Windows")).click();
+//		
+//		d.findElement(By.xpath("//button[.='Shop Now']")).click();
+//		
+//		Set<String> allwinid = d.getWindowHandles();
+//		
+//		int count = allwinid.size();
+//		
+//		System.out.println(count);
+//		
+//		for (String win : allwinid) {
+//			if (!pwin.equals(win)) {
+//				d.switchTo().window(win);
+//				d.close();
+//			}
+//			System.out.println(win);
+//		}
+//		
+		
 		
 		
 		
